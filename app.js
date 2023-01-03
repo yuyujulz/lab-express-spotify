0a3474caa720be338fd7f3fa5b0de5bd570a0a0c
Julia Ganina <ganina.julia2@gmail.com>
2023-01-03 17:37:41 +0000

    done Iteration 1-2 and got stuck on Iteration3 of creating the path.

--- app.js
+++ app.js
@@ -2,17 +2,44 @@ require('dotenv').config();
 
 const express = require('express');
 const hbs = require('hbs');
-
-// require spotify-web-api-node package here:
-
 const app = express();
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-// setting the spotify-api goes here:
+// require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  });
+  
+  // Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
+
+app.get('/artist-search', (req, res) => {
+    spotifyApi
+      .searchArtists(req.query.artist)
+      .then(data => {
+      console.log('The received data from the API: ', data.body.name);
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+     
+      })
+      .catch(err => console.log('The error while searching artists occurred: ', err))
+  });
+  
+  
 
 // Our routes go here:
+
+app.get("/home", (req, res) => res.render("home"));
+app.get('/artist-search-results', (req, res) => res.render('artist-search-results'));
+app.get('/albums', (req, res) => res.render('albums'));
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
